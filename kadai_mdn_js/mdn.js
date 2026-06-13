b5f88345e3492ac1e9cb4c36de2d744e092ab56f
hahakogusa <ten.gachimayar@gmail.com>
2026-06-13 09:38:04 +0900

    kadai_mdn_jsを変更

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const date = new Date();
-const year = date.getFullYear(2026);
-const month = date.getMonth(6) + 1; // getMonth()は0から始まるので+1します
-const day = date.getDate(11);
+const year = date.getFullYear();
+const month = date.getMonth() + 1; // getMonth()は0から始まるので+1します
+const day = date.getDate();
 
 console.log(year + '年' + month + '月' + day + '日');
